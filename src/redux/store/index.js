c245ae73b7de645816cb65cc43ecce69e7315dc5
33orange <oranges.vladislav@gmail.com>
2021-10-22 15:55:28 +0300

    fix

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,7 +1,5 @@
-import { applyMiddleware, createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
+import { createStore } from 'redux';
 import reducer from '../reducer';
 
-const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(reducer);
 export default store;
