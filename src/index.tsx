7a59190d3c5aa89a2dc368c427b73fa6fa6aca3c
33orange <oranges.vladislav@gmail.com>
2021-10-25 12:05:19 +0300

    typed

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import { render } from 'react-dom';
 import TodosList from './components/TodosList';
 import { Provider } from 'react-redux';
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, './src/index.js'),
+  entry: path.resolve(__dirname, './src/index.tsx'),
   resolve: {
     extensions: ['*', '.js', '.jsx', '.tsx', '.ts'],
   },
